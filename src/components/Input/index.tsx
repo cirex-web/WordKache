386770cbeb6362cf4bfb0607cca0c86bf9611147
cirex-web <erxu@ctemc.org>
2023-05-03 13:03:09 -0400

    search box done

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,5 @@
-import css from "./index.module.css"
-interface IInputProps extends React.HTMLProps<HTMLInputElement>{}
-export const Input = ({...rest}: IInputProps) =>{
-    return <input {...rest} className={css.input}>
-    </input>
-}
+import css from "./index.module.css";
+interface IInputProps extends React.HTMLProps<HTMLInputElement> {}
+export const Input = ({ className, ...rest }: IInputProps) => {
+  return <input {...rest} className={css.input + " " + (className??"")}></input>;
+};
--- src/features/WordTable/TableHeader/index.tsx
+++ src/features/WordTable/TableHeader/index.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import { Icon } from "../../../components/Icon";
+import { Input } from "../../../components/Input";
 import { Text } from "../../../components/Text";
 import css from "./index.module.css";
-export const TableHeader = ({ folderName }: { folderName: string }) => {
+export const TableHeader = ({
+  folderName,
+  setSearchInput,
+}: {
+  folderName: string;
+  setSearchInput: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [inputOpen, setInputOpen] = useState(false);
+
   return (
     <div className={css.header}>
       <Text type="heading" bold noWrap className={css.heading}>
         {folderName}
+      
         <span className={css.buttonsTray}>
           <div className={css.buttons}>
-            <Icon name="search" />
-
+            <div
+              className={css.inputContainer}
+              style={{ flexGrow: inputOpen ? 1 : 0 }}
+            >
+              <Input
+                placeholder="Search cards..."
+                onChange={(event) => setSearchInput(event.currentTarget.value)}
+              />
+            </div>
+            <Icon name="search" onClick={() => setInputOpen(!inputOpen)} />
             <Icon name="ios_share" />
           </div>
         </span>
