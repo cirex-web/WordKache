6f63b2ddc10e0be802b1e6d962d25f84eb1e42db
topshenyi-web <erxu@ctemc.org>
2023-06-19 12:40:50 -0400

    transfer hidden cards to new folder

--- src/chromeServices/background/alarms.ts
+++ src/chromeServices/background/alarms.ts
@@ -1,4 +1,6 @@
-import { addData } from "../../utils/firebase";
+import { nanoid } from "nanoid";
+import { Card, Folder } from "../../types/storageTypes";
+import { addData, fetchData } from "../../utils/firebase";
 import { ChromeStorage } from "../../utils/storage/storage";
 import { logger } from "../logger";
 
@@ -8,7 +10,7 @@ export const uploadStorage = async (alarm: chrome.alarms.Alarm) => {
     logger.info("Uploading to firebase...");
     if ("userId" in allData && typeof allData.userId === "string") {
         logger.info("Found userId", allData.userId);
-        await addData(allData.userId, allData);
+        await addData("users", allData.userId, { ...allData, lastUpdated: +new Date() });
     }
 }
 export const preloadHTML = async (alarm: chrome.alarms.Alarm) => {
@@ -21,6 +23,30 @@ export const preloadHTML = async (alarm: chrome.alarms.Alarm) => {
         });
         logger.info("Set up hidden HTML page for faster load times!");
     }
+}
+export const makeHiddenCardFolder = async (alarm: chrome.alarms.Alarm) => {
+    if (alarm.name !== "checkHiddenCards") return;
+
+    logger.info("Checking hidden card status...");
+    const userId = await ChromeStorage.get("userId") as string | undefined ?? "";
+    const formData: { url?: string, done?: boolean, newFolderMade?: boolean } = await fetchData("forms", userId) ?? {};
+    logger.info(formData);
+    if (formData.done && !formData.newFolderMade) {
+        logger.info("Making hidden card folder...");
+        const folders = ((await ChromeStorage.get("folders")) ?? []) as Folder[];
+        const cards = ((await ChromeStorage.get("cards")) ?? []) as Card[];
+        const hiddenFolderId = nanoid();
+        for (const card of cards) {
+            if (card.hidden) {
+                delete card.hidden;
+                card.location = hiddenFolderId;
+            }
+        }
+        folders.push({ id: hiddenFolderId, name: "Hidden Cards" });
+        await ChromeStorage.set({ cards, folders });
+        addData("forms", userId, { ...formData, newFolderMade: true });
+    }
+
 }
 
 export const addAlarm = async (name: string, alarmInfo: chrome.alarms.AlarmCreateInfo) => {
