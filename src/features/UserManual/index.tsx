102960d58fc1b89bbd982d46ad67587d6b008621
topshenyi-web <erxu@ctemc.org>
2023-06-20 13:08:34 -0400

    bump version to 0.8.1

--- src/features/UserManual/index.tsx
+++ src/features/UserManual/index.tsx
@@ -61,15 +61,8 @@ export const UserManual = () => {
           I have feedback/questions!
         </Text>
         <Text type="paragraph">
-          We're actively monitoring all responses on{" "}
-          <a
-            href="https://forms.gle/bkos6SGzr6Jeo33n6"
-            target="_blank"
-            rel="noreferrer"
-          >
-            this form
-          </a>
-          .
+          Send an email to "cirex314@gmail.com"! We'd love to hear your
+          thoughts.
         </Text>
         <Text type="heading" style={{ marginTop: "15px" }} bold>
           I've completed the beta testing form! Where are my hidden cards?
